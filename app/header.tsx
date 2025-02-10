70aba1653b4e26198249f8b1e6d1a04309fbfab6
fedelombar <lombardozzifederico@outlook.com>
2025-02-10 18:22:42 -0300

    :rocket: add github redirect

--- app/header.tsx
+++ app/header.tsx
@@ -16,6 +16,16 @@ export function Header() {
         >
           About
         </Link>
+
+        <a
+          href="https://github.com/fedelombar"
+          target="_blank"
+          className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] items-center p-2 rounded-sm transition-[background-color] whitespace-nowrap -mr-2"
+        >
+          <GitHubIcon />
+          <span className="hidden md:inline">&nbsp;Github</span>
+        </a>
+
         <a
           href="https://x.com/lombarrr"
           target="_blank"
@@ -48,3 +58,22 @@ function TweetIcon(props: any) {
     </svg>
   );
 }
+
+function GitHubIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.68-.22.68-.48v-1.69c-2.78.6-3.37-1.2-3.37-1.2-.45-1.15-1.1-1.46-1.1-1.46-.9-.62.07-.6.07-.6 1 .07 1.53 1.04 1.53 1.04.9 1.53 2.36 1.1 2.94.84.09-.66.35-1.1.63-1.35-2.22-.25-4.56-1.12-4.56-4.97 0-1.1.4-2 1.04-2.7-.1-.26-.46-1.28.1-2.67 0 0 .84-.27 2.75 1.03a9.56 9.56 0 0 1 5 0c1.9-1.3 2.75-1.03 2.75-1.03.56 1.4.2 2.42.1 2.67.64.7 1.04 1.6 1.04 2.7 0 3.86-2.34 4.72-4.57 4.96.36.3.68.9.68 1.8v2.67c0 .26.18.57.68.48A10.01 10.01 0 0 0 22 12c0-5.52-4.48-10-10-10Z"
+      />
+    </svg>
+  );
+}
+
